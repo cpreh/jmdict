a48e67ef84b81ad8881892b267171b8b690737f4
Philipp Reh <sefi@s-e-f-i.de>
2011-12-25 13:56:01 +0000

    Added missing include to unistd.h

--- jmdict.cpp
+++ jmdict.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdexcept>
 #include <exception>
 #include <memory>
+#include <unistd.h>
 #include "sqlite.h"
 #include "kana2romaji.h"
 using namespace std;
